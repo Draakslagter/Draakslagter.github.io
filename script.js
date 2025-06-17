e0a01844219507beb67a9a3c12402701658f0f06
Draakslagter <simon@gssr.co.za>
2025-06-17 16:56:25 +0200

    Finished pages

---
+++ script.js
@@ -0,0 +1,6 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+function CopyMail() {
+  navigator.clipboard.writeText("simon@gssr.co.za");
+}
